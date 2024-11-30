3a425d5f14c2bf3af3c983982a54c06803ae66f6
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2024-12-01 03:08:12 +0800

    feat: implement loading

--- dashboard/src/routes/index.tsx
+++ dashboard/src/routes/index.tsx
@@ -1,12 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/features/dashboard/components/side-bar-components/app-siderbar";
-import { DashboardSideBar } from "./child.lazy";
+// import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+// import { AppSidebar } from "@/features/dashboard/components/side-bar-components/app-siderbar";
+// import { DashboardSideBar } from "./child.lazy";
+import { CircularProgress } from "@nextui-org/react";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 export function Index() {
-  return <>Hello</>;
+  return (
+    <div className="flex items-center justify-center">
+      <CircularProgress size="lg" aria-label="Loading..." />
+    </div>
+  );
 }
