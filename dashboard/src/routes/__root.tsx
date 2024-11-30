9d24d23ebae5a79b5cd1efe041eef4f9cc625f1f
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2024-12-01 03:00:45 +0800

    feat: done implementing title page

--- dashboard/src/routes/__root.tsx
+++ dashboard/src/routes/__root.tsx
@@ -3,6 +3,9 @@ import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { ThemeProvider } from "@/components/theme-provider";
 import "@/index.css";
 import { DashboardSideBar } from "./child.lazy";
+import { Switch } from "@nextui-org/react";
+import { PageTitle } from "@/components/shared/page-title";
+import { Sun, Moon, Bell, Cog } from "lucide-react";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -14,8 +17,27 @@ function RootComponent() {
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <DashboardSideBar
           children={
-            <div className="flex-1 w-full">
-              <Outlet />
+            <div className="flex flex-col w-full gap-6">
+              <div className="flex justify-between items-center mx-20">
+                <PageTitle
+                  title="Dashboard"
+                  description="Welcome to Dashboard"
+                />
+                <div className="flex flex-row justify-around items-center gap-4">
+                  <Switch
+                    defaultSelected
+                    size="lg"
+                    color="success"
+                    startContent={<Sun />}
+                    endContent={<Moon />}
+                  ></Switch>
+                  <Bell />
+                  <Cog />
+                </div>
+              </div>
+              <div>
+                <Outlet />
+              </div>
             </div>
           }
         />
