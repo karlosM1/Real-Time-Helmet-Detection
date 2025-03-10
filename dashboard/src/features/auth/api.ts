e3eaee1e9aa2e8a4ae44a459c67685ef1da2ae5d
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2025-03-10 17:33:39 +0800

    feat: intial server side

--- dashboard/src/features/auth/api.ts
+++ dashboard/src/features/auth/api.ts
@@ -1,38 +1,119 @@
 import {
-  OAuthProvider,
+  GoogleAuthProvider,
   User,
+  createUserWithEmailAndPassword,
   onAuthStateChanged,
   signInWithEmailAndPassword,
-  signInWithRedirect,
+  signInWithPopup,
+  signOut,
 } from "firebase/auth";
-
 import { auth } from "@/lib/firebaseConfig";
-import { AuthUser } from "./types";
+// import { AuthUser } from "./types";
+import { createContext, useContext, useEffect, useState } from "react";
 
-export const getAuthUser = async (): Promise<AuthUser | null> => {
-  try {
-    const attributes = (await new Promise((resolve) => {
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-        unsubscribe();
-        resolve(user);
-      });
-    })) as User | null;
+interface IUserAuthProviderProps {
+  children: React.ReactNode;
+}
 
-    if (!attributes?.email) {
-      return null;
-    }
-
-    const user = {
-      email: attributes.email,
-      password: "",
-      uid: attributes.uid,
-      apiToken: "",
-    } satisfies AuthUser;
-
-    return user;
-  } catch (error) {
-    console.log(error);
-  }
-
-  return null;
+type AuthContextData = {
+  user: User | null;
+  logIn: typeof logIn;
+  signUp: typeof signUp;
+  logOut: typeof logOut;
+  googleSignIn: typeof googleSignIn;
 };
+
+const logIn = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+const signUp = (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+const logOut = () => {
+  signOut(auth);
+};
+
+const googleSignIn = () => {
+  const googleAuthProvider = new GoogleAuthProvider();
+  return signInWithPopup(auth, googleAuthProvider);
+};
+
+export const userAuthContext = createContext<AuthContextData>({
+  user: null,
+  logIn,
+  signUp,
+  logOut,
+  googleSignIn,
+});
+
+export const UserAuthProvider: React.FunctionComponent<
+  IUserAuthProviderProps
+> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("I am in useEffect and user is : ", user);
+      if (user) {
+        console.log("The logged in user state is : ", user);
+        setUser(user);
+      }
+
+      return () => {
+        unsubscribe();
+      };
+    });
+  });
+  const value: AuthContextData = {
+    user,
+    logIn,
+    signUp,
+    logOut,
+    googleSignIn,
+  };
+
+  return (
+    <userAuthContext.Provider value={value}>
+      {children}
+    </userAuthContext.Provider>
+  )
+};
+
+export const useUserAuth = () => {
+  return useContext(userAuthContext);
+};
+
+
+
+
+
+
+// export const getAuthUser = async (): Promise<AuthUser | null> => {
+//   try {
+//     const attributes = (await new Promise((resolve) => {
+//       const unsubscribe = onAuthStateChanged(auth, (user) => {
+//         unsubscribe();
+//         resolve(user);
+//       });
+//     })) as User | null;
+
+//     if (!attributes?.email) {
+//       return null;
+//     }
+
+//     const user = {
+//       email: attributes.email,
+//       password: "",
+//       uid: attributes.uid,
+//       apiToken: "",
+//     } satisfies AuthUser;
+
+//     return user;
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+//   return null;
+// };
